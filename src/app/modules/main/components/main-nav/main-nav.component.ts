75aa94f2e5d2cc745cad279a01c2359889175997
Simon Lepel <simbo@simbo.de>
2016-08-07 18:13:59 +0200

    update main-nav template (use dropdown directives and add styles)

--- src/app/modules/main/components/main-nav/main-nav.component.ts
+++ src/app/modules/main/components/main-nav/main-nav.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
+import {Component, ElementRef} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+
+import {DROPDOWN_DIRECTIVES, DROPDOWN_SERVICES} from "../../../common/directives/dropdown";
 
 @Component({
   selector: 'main-nav',
-  directives: [...ROUTER_DIRECTIVES],
   templateUrl: './main-nav.template',
+  styleUrls: ['./main-nav.styles'],
+  directives: [
+    ...DROPDOWN_DIRECTIVES,
+    ...ROUTER_DIRECTIVES
+  ],
+  providers: [
+    ...DROPDOWN_SERVICES
+  ]
 })
 
 export class MainNavComponent {
