c5312c3ce03aedd241fc6d5273e03892273bef0f
Simon Lepel <simbo@simbo.de>
2016-08-14 05:29:22 +0200

    prefix component selectors

--- src/app/modules/main/components/content-about/content-about.component.ts
+++ src/app/modules/main/components/content-about/content-about.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'content-about',
+  selector: 'c-content-about',
   templateUrl: './content-about.template'
 })
 
--- src/app/modules/main/components/content-register/content-register.component.ts
+++ src/app/modules/main/components/content-register/content-register.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'content-home',
+  selector: 'c-content-home',
   templateUrl: './content-register.template'
 })
 
