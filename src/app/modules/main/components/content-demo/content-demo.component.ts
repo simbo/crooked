c2b814a7468ead736f2a82b394b592c827eb7f5e
Simon Lepel <simbo@simbo.de>
2016-08-14 05:34:16 +0200

    add component content-demo

---
+++ src/app/modules/main/components/content-demo/content-demo.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'c-content-demo',
+  templateUrl: './content-demo.template'
+})
+
+export class ContentDemoComponent {
+
+  constructor() {}
+
+}
